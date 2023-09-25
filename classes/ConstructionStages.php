dd97beb624390e0270cb5debbb7aa2a4a03fdc4d
atamuradow.arthur <atamuradow.arthur@gmail.com>
2023-09-25 19:20:09 +0500

    adding comments

--- classes/ConstructionStages.php
+++ classes/ConstructionStages.php
@@ -57,6 +57,11 @@ class ConstructionStages
 			return ['error' => 'Validation failed', 'validationErrors' => $validationErrors];
 		}
 
+		$duration = $this->calculateDuration($data);
+
+
+		$data->duration = $duration;
+
 		$stmt = $this->db->prepare("
 			INSERT INTO construction_stages
 			    (name, start_date, end_date, duration, durationUnit, color, externalId, status)
@@ -76,11 +81,18 @@ class ConstructionStages
 		return $this->getSingle($this->db->lastInsertId());
 	}
 
+	    /**
+     * Update a construction stage partially.
+     *
+     * @param int $id   The ID of the construction stage to update.
+     * @param object $data Data containing the fields to update.
+     *
+     * @return array Associative array representing the updated construction stage.
+     */
+
     public function patch(ConstructionStagesUpdate $data, $id)
     {
 
-
-		
 
         $existingStage = ($this->getSingle($id))[0];
 	
@@ -141,6 +153,15 @@ class ConstructionStages
         return $this->getSingle($id);
     }
 
+
+	 /**
+     * Mark a construction stage as deleted.
+     *
+     * @param int $id The ID of the construction stage to mark as deleted.
+     *
+     * @return array Associative array with a message indicating the stage has been marked as deleted.
+     */
+
     public function delete($id)
     {
 
@@ -160,7 +181,14 @@ class ConstructionStages
         return ['message' => 'Construction stage marked as DELETED'];
     }
 
-
+    /**
+     * Validate construction stage data against specified rules.
+     *
+     * @param array $data Data to validate.
+     *
+     * @return array Associative array of validation errors, if any.
+     */
+	
 	public function validateConstructionStageData($data)
     {
 
@@ -180,7 +208,7 @@ class ConstructionStages
         if (isset($data['endDate']) && $data['endDate'] !== null) {
             if (!preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/', $data['endDate'])) {
                 $errors['endDate'] = 'Invalid endDate format. Use ISO8601 format (e.g., 2022-12-31T14:59:00Z).';
-            } elseif (strtotime($data['endDate']) <= strtotime($data['endDate'])) {
+            } elseif (strtotime($data['endDate']) <= strtotime($data['startDate'])) {
                 $errors['endDate'] = 'End date must be later than the start date.';
             }
         }
@@ -211,4 +239,42 @@ class ConstructionStages
         return $errors;
     }
 
+
+	 /**
+     * Calculate the duration of a construction stage based on startDate, endDate, and durationUnit.
+     *
+     * @param ConstructionStagesCreate $data Data containing startDate, endDate, and durationUnit.
+     *
+     * @return float|null Calculated duration or null if not applicable.
+     */
+	
+	private function calculateDuration($data)
+	{
+		
+		if (!isset($data->endDate) || $data->endDate === null) {
+			return null;
+		}
+
+		$startDate = new DateTime($data->startDate);
+		$endDate = new DateTime($data->endDate);
+
+	
+		$durationUnit = isset($data->durationUnit) ? $data->durationUnit : 'DAYS';
+
+		if ($durationUnit === 'HOURS') {
+		
+			$duration = $startDate->diff($endDate)->h;
+		} elseif ($durationUnit === 'WEEKS') {
+		
+			$duration = $startDate->diff($endDate)->days / 7;
+		} else {
+	
+			$duration = $startDate->diff($endDate)->days;
+		}
+
+
+		$duration = max(0, round($duration, 2));
+
+		return $duration;
+	}	
 }
